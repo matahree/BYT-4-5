9e1bc74895d78149271e5e95fd3c76b5b975ce28
Mirac Gulbeyaz <95246970+matahree@users.noreply.github.com>
2022-11-22 12:50:40 +0100

    Add files via upload

---
+++ Calculator/src/DTO/Response.java
@@ -0,0 +1,14 @@
+package DTO;
+
+public class Response {
+    private final int result;
+
+    public Response(int result) {
+        this.result = result;
+    }
+
+    public int getResult() {
+        return result;
+    }
+}
+ 
---
+++ Pool/src/SomeMysteriousObject.java
@@ -0,0 +1,11 @@
+public class SomeMysteriousObject {
+
+    int someInt;
+
+    public SomeMysteriousObject() {
+        this.someInt = 2846;
+    }
+
+
+}
+ 
